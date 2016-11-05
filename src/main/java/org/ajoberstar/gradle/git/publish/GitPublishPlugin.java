96fc89945b17a0c0c185f40f07a2fd9879bbaa47
Andrew Oberstar <ajoberstar@gmail.com>
2016-11-05 15:52:30 -0500

    placeholder plugin class

---
+++ src/main/java/org/ajoberstar/gradle/git/publish/GitPublishPlugin.java
@@ -0,0 +1,11 @@
+package org.ajoberstar.gradle.git.publish;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class GitPublishPlugin implements Plugin<Project> {
+    @Override
+    public void apply(Project project) {
+
+    }
+}
