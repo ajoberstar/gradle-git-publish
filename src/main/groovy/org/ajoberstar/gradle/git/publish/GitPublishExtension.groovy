b7f36d7ea50b5912f7c09ed4aab50955003b5d27
Andrew Oberstar <ajoberstar@gmail.com>
2016-11-05 20:32:35 -0500

    first untested pass at an impl

---
+++ src/main/groovy/org/ajoberstar/gradle/git/publish/GitPublishExtension.groovy
@@ -0,0 +1,29 @@
+package org.ajoberstar.gradle.git.publish
+
+import groovy.transform.PackageScope
+
+import org.ajoberstar.grgit.Grgit
+import org.gradle.api.Action
+import org.gradle.api.tasks.util.PatternFilterable
+import org.gradle.api.tasks.util.PatternSet
+
+class GitPublishExtension {
+    String repoUri
+
+    String branch
+
+    File repoDir
+
+    final PatternFilterable preserve = new PatternSet()
+
+    @PackageScope
+    Grgit repo
+
+    public GitPublishExtension() {
+        preserve.include('.git')
+    }
+
+    void preserve(Action<? super PatternFilterable> action) {
+        action.execute(preserve)
+    }
+}
