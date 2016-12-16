dc98e64f9c6dbc20cc1413aa363b0d73441313e1
Andrew Oberstar <ajoberstar@gmail.com>
2016-12-16 17:32:53 -0600

    Allow copy to be configured from the extension
    This resolves #8.

--- src/main/groovy/org/ajoberstar/gradle/git/publish/GitPublishExtension.groovy
+++ src/main/groovy/org/ajoberstar/gradle/git/publish/GitPublishExtension.groovy
@@ -1,6 +1,8 @@
 package org.ajoberstar.gradle.git.publish
 
 import org.gradle.api.Action
+import org.gradle.api.Project
+import org.gradle.api.file.CopySpec
 import org.gradle.api.tasks.util.PatternFilterable
 import org.gradle.api.tasks.util.PatternSet
 
@@ -11,10 +13,18 @@ class GitPublishExtension {
 
     File repoDir
 
-    final PatternFilterable preserve = new PatternSet()
+    final CopySpec contents
 
-    public GitPublishExtension() {
+    final PatternFilterable preserve
+
+    public GitPublishExtension(Project project) {
+        contents = project.copySpec()
+        preserve = new PatternSet()
         preserve.include('.git')
+    }
+
+    void contents(Action<? super CopySpec> action) {
+        action.execute(contents)
     }
 
     void preserve(Action<? super PatternFilterable> action) {
