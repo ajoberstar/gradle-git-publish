67a123b52a6c913abddadc81cd41b6b5818adc86
Andrew Oberstar <ajoberstar@gmail.com>
2018-04-19 22:59:33 -0500

    Fix task dependencies with concrete tasks
    Implement real task classes instead of ad-hoc ones. This allows proper
    declaration of inputs/outputs. Generally, the intent is to ensure Gradle
    knows the repo directory is managed by these tasks. This allows us to
    restore support for implicit task dependencies when someone uses the
    contents copy spec:
    
      task createOutput { // output some stuff }
    
      gitPublish {
        contents {
          from createOutput
        }
      }
    
    This drops support for Gradle < 4.3 due to requirement of Provider APIs
    for lazy configuration.
    
    This fixes #41.

---
+++ src/main/groovy/org/ajoberstar/gradle/git/publish/tasks/GitPublishPush.java
@@ -0,0 +1,65 @@
+package org.ajoberstar.gradle.git.publish.tasks;
+
+import org.ajoberstar.grgit.BranchStatus;
+import org.ajoberstar.grgit.Grgit;
+import org.gradle.api.DefaultTask;
+import org.gradle.api.model.ObjectFactory;
+import org.gradle.api.provider.Property;
+import org.gradle.api.tasks.*;
+
+import javax.inject.Inject;
+import java.io.File;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class GitPublishPush extends DefaultTask {
+  private final Property<Grgit> grgit;
+  private final Property<String> branch;
+
+  @Inject
+  public GitPublishPush(ObjectFactory objectFactory) {
+    this.grgit = objectFactory.property(Grgit.class);
+    this.branch = objectFactory.property(String.class);
+
+    // always consider this task out of date
+    this.getOutputs().upToDateWhen(t -> false);
+
+    this.onlyIf(t -> {
+      try {
+        Grgit git = getGrgit().get();
+        BranchStatus status = git.getBranch().status(op -> {
+          op.setName(getBranch().get());
+        });
+        return status.getAheadCount() > 0;
+      } catch (IllegalStateException e) {
+        // if we're not tracking anything yet (i.e. orphan) we need to push
+        return true;
+      }
+    });
+  }
+
+  @Internal
+  public Property<Grgit> getGrgit() {
+    return grgit;
+  }
+
+  @Input
+  public Property<String> getBranch() {
+    return branch;
+  }
+
+  @OutputDirectory
+  public File getRepoDirectory() {
+    return getGrgit().get().getRepository().getRootDir();
+  }
+
+  @TaskAction
+  public void push() {
+    Grgit git = getGrgit().get();
+    String pubBranch = getBranch().get();
+    git.push(op -> {
+      op.setRefsOrSpecs(Arrays.asList(String.format("refs/heads/%s:refs/heads/%s", pubBranch, pubBranch)));
+    });
+  }
+}
