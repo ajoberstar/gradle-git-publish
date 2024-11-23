86c93573c3a627278d8bfcdeb60100aff1e6641f
Andrew Oberstar <andrew@ajoberstar.org>
2024-11-23 14:40:50 -0600

    patch: Suppress error output in value source
    Might regret this, but it's confusing to print out fatal errors here
    when we handle the result either way. It's normal that some of these
    value source's might fail, e.g. if your project isn't running from a
    Git repo.

--- src/main/java/org/ajoberstar/gradle/git/publish/GitCliValueSource.java
+++ src/main/java/org/ajoberstar/gradle/git/publish/GitCliValueSource.java
@@ -1,6 +1,7 @@
 package org.ajoberstar.gradle.git.publish;
 
 import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 
 import javax.inject.Inject;
@@ -27,6 +28,7 @@ public abstract class GitCliValueSource implements ValueSource<String, GitCliVal
         spec.executable("git");
         spec.setArgs(getParameters().getGitArguments().get());
         spec.setStandardOutput(output);
+        spec.setErrorOutput(OutputStream.nullOutputStream());
       });
       return output.toString(StandardCharsets.UTF_8).trim();
     } catch (Exception e) {
